c983bf63c4f8c5c1174f4133b194bffab8fe1040
unknown <x65949@USMANBUX65949.usma.ds.army.edu>
2015-01-30 11:50:24 -0500

    switched to dj_database_url

--- lesson09/settings.py
+++ lesson09/settings.py
@@ -55,12 +55,10 @@ WSGI_APPLICATION = 'lesson09.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default="sqlite:///"+os.path.join(BASE_DIR, 'db.sqlite3')),    
 }
 
 # Internationalization
